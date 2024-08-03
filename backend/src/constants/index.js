03be3c790c4fe37b468e79ae3d36619953a4b60b
Abhishek Naithani <abhinaithani2@gmail.com>
2024-08-04 02:39:39 +0530

    local backend connection to deployed frontend

--- backend/src/constants/index.js
+++ backend/src/constants/index.js
@@ -1,5 +1,5 @@
-// const frontend_URL = `https://whatsapp-msz-scheduler-frontend.onrender.com`;
-const frontend_URL = `http://localhost:5173`;
+const frontend_URL = `https://whatsapp-message-scheduler-frontend.onrender.com`;
+// const frontend_URL = `http://localhost:5173`;
 
 module.exports = {
     frontend_URL
--- frontend/src/constants/index.js
+++ frontend/src/constants/index.js
@@ -1,5 +1,5 @@
-// export const Banckend_URL = `https://whatsapp-msz-scheduler-backend.onrender.com/api/v1`;
+// export const Banckend_URL = `https://whatsapp-message-scheduler-backend.onrender.com/api/v1`;
 export const Banckend_URL = `http://localhost:3000/api/v1`;
 
-// export const SOCKET_SERVER_URL = `https://whatsapp-msz-scheduler-backend.onrender.com`;
+// export const SOCKET_SERVER_URL = `https://whatsapp-message-scheduler-backend.onrender.com`;
 export const SOCKET_SERVER_URL = `http://localhost:3000`;
